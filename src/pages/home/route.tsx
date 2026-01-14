800e5b2a9c3b723cf7947f49e0c18f706efcb286
Paradox117 <naireetsadhukhan117@gmail.com>
2026-01-14 20:14:43 +0530

    route changes

--- src/pages/home/route.tsx
+++ src/pages/home/route.tsx
@@ -1,10 +1,10 @@
-// import { Dashboard } from "./sections/dashboard/Dashboard"
+import { Dashboard } from "./sections/dashboard/Dashboard"
 import MeetOurTeam from "./sections/meet-our-team/MeetOurTeam"
 
 export const HomeRoute = () => {
 	return (
 		<div className="w-full min-h-screen bg-[#131313]">
-		{/* <Dashboard /> */}
+		<Dashboard /> 
 		<MeetOurTeam/>
 		</div>
 	)
