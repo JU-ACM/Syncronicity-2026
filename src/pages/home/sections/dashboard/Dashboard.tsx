994186e5c305ede1ec19c33a173342b1927e9690
Dipram Biswas <dipramb9090@gmail.com>
2026-03-05 12:25:40 +0530

    Changed hero section for the animations

--- src/pages/home/sections/dashboard/Dashboard.tsx
+++ src/pages/home/sections/dashboard/Dashboard.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import herobg from '../../../../assets/dashboard/hero-bg.png';
+import React from "react";
+import herobg from "../../../../assets/dashboard/hero-bg.png";
+import heroimg from "../../../../assets/dashboard/hero-img.png";
 
-import ListofLinks from './ListofLinks';
-import Description from './Description';
-import { EventCard } from './EventCard';
-import Navbar from '../../../../../components/Navbar'
-import Clouds from './Clouds';
-import Robot from './Robot';
-
-
+import ListofLinks from "./ListofLinks";
+import Description from "./Description";
+import { EventCard } from "./EventCard";
+import Navbar from "../../../../../components/Navbar";
+import Clouds from "./Clouds";
 
 export const Dashboard: React.FC = () => {
   return (
@@ -30,51 +28,43 @@ export const Dashboard: React.FC = () => {
         backgroundImage: `url(${herobg})`,
       }}
     >
-		<Navbar />
-      <section className="h-screen w-full flex justify-center items-center relative">
+      <Navbar />
+      <section className="h-screen w-full relative">
         {/* Left Event Card */}
-        <EventCard 
-				className="relative scale-75 md:scale-100 left-40 md:left-8 top-1/2 -translate-y-1/2 mt-[75%] md:mt-0"
-				title="Synchronicity S2"
-				duration="10-hour · on-site"
-				eventType="hackathon"
-				prizePool="₹23,000 prize pool"
-				date="11 April 2025"
-				tiltDirection="left"
-			  />
+        <EventCard
+          className="absolute hidden lg:block left-15 top-60 cursor-pointer"
+          title="Synchronicity S2"
+          duration="10-hour · on-site"
+          eventType="hackathon"
+          prizePool="₹23,000 prize pool"
+          date="11 April 2025"
+          tiltDirection="left"
+        />
 
-        {/* Container for title, robot, link, description, and join button */}
-        <div className=" w-full lg:w-[55%] h-[80%] flex flex-col items-center z-10 ">
-          <div className='h-[40%] w-[100%] font-black font-Bounded text-sm md:text-xl flex items-center  '>
-            <h1 className='text-white text-center px-12 md:px-0 '>
-              Your Gateway To The Global Computing Community
-            </h1>
-          </div>
-          
-          <div className='h-[40%] w-full overflow-y-visible grid grid-rows-2 md:grid-cols-3'>
-            <ListofLinks/>
-	  		<Robot/>
+        <p className="absolute md:top-15 top-30 inset-x-0 font-bounded lg:text-8xl md:text-6xl text-4xl text-white text-center px-12 md:px-0 ">
+          Synchronicity
+          <br />
+          <span className="text-blue-600">Season 2</span>
+        </p>
 
+        <ListofLinks className="hidden md:flex lg:left-80 lg:top-80 md:left-40 top-60" />
 
-            <Description/>
-          </div>
-        </div>
+        <img className="absolute left-1/2 -translate-x-1/2 lg:top-45 md:top-35 top-50 lg:w-120 md:w-100 w-70 object-contain" src={heroimg} alt="robot image" />
+
+        <Description className="lg:right-80 lg:top-79 md:right-40 top-59" />
 
         {/* Right Event Card */}
-        <EventCard 
-				className="relative scale-75 md:scale-100 right-40 md:right-8 top-1/2 -translate-y-1/2 mt-[75%] md:mt-0"
-				title="CodeFest 2025"
-				duration="24-hour · virtual"
-				eventType="coding competition"
-				prizePool="₹50,000 prize pool"
-				date="25 May 2025"
-				tiltDirection="right"
-			  />
+        <EventCard
+          className="absolute hidden lg:block right-15 top-60 cursor-pointer"
+          title="CodeFest 2025"
+          duration="24-hour · virtual"
+          eventType="coding competition"
+          prizePool="₹50,000 prize pool"
+          date="25 May 2025"
+          tiltDirection="right"
+        />
       </section>
-	  <Clouds/>
-
-    
-	  
+      <Clouds />
     </div>
   );
 };
--- src/pages/home/sections/dashboard/Description.tsx
+++ src/pages/home/sections/dashboard/Description.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
-function Description() : React.JSX.Element  {
+function Description({className = ""}): React.JSX.Element {
   return (
-    <div className='md:w-[200%] lg:w-full text-black font-euclid text-base leading-4.5 flex flex-col justify-start items-end md:items-start  text-center md:text-left px-12 p-8 md:p-0 md:px-0 scale-80 md:scale-100 mt-[30%] md:mt-[10%] lg:mt-0 '>ACM-JU is a student-run technical community focused on learning beyond textbooks. We organize workshops, seminars, coding events, and flagship programs to help students explore real-world computing, professional growth—while staying connected to a global ecosystem.</div>
-  )
+    <div className={` ${className} absolute lg:w-70 w-50 text-black font-euclid lg:text-base md:text-sm text-center md:text-left`}>
+      ACM-JU is a student-run technical community focused on learning beyond
+      textbooks. We organize workshops, seminars, coding events, and flagship
+      programs to help students explore real-world computing, professional
+      growth—while staying connected to a global ecosystem.
+    </div>
+  );
 }
 
-export default Description
+export default Description;
